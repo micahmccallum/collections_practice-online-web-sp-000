94a1d5c65a779982127d88e369f56620f365bf90
Micah McCallum <micahmccallum@outlook.com>
2020-03-27 15:45:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,12 +5,14 @@ end
 
 def sort_array_desc(array)
   array.sort do |a, b|
-    b<=>a
+    b <=> a
   end
 end
 
 def sort_array_char_count(array)
-
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
 end
 
 def swap_elements(array)
